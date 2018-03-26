3b48720908a4c9765e8901973bff17ee187db90e
Kurt Maurer <kurtrm@gmail.com>
2018-03-26 14:08:25 -0700

    Added doc strings to distance functions.

--- src/distance_funcs.py
+++ src/distance_funcs.py
@@ -5,6 +5,9 @@ import numpy as np
 
 
 def cosine_distance(x: np.ndarray, other: np.ndarray) -> float:
+    """
+    Calculates the cosine distance between two vectors.
+    """
     dot_product = other.dot(x.reshape(-1, 1))
     mag_x = np.sqrt(np.sum(x**2))
     mag_other = np.sqrt(np.sum(other**2))
@@ -13,4 +16,7 @@ def cosine_distance(x: np.ndarray, other: np.ndarray) -> float:
 
 
 def euclidean_distance(x: np.ndarray, other: np.ndarray) -> float:
+    """
+    Calculates the euclidean distance between two data sets.
+    """
     return np.sqrt(np.sum((other - x)**2, axis=1))
