e1084ebe91186eb8a0ea9cd5544eca30d3d5fd1c
Kurt Maurer <kurtrm@gmail.com>
2018-03-26 14:12:31 -0700

    Loading the test file with imports.

---
+++ tests/test_kNN.py
@@ -0,0 +1,7 @@
+"""
+Test the kNN class and distance functions.
+"""
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from src.kNN import KNearestNeighbors
