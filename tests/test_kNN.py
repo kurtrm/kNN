d3ffc3635144e7f2541daa217a046523e9e0e823
Kurt Maurer <kurtrm@gmail.com>
2018-05-31 21:46:57 -0700

    Got the testing set up.

--- tests/test_kNN.py
+++ tests/test_kNN.py
@@ -4,4 +4,12 @@ Test the kNN class and distance functions.
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from src.distance_funcs import cosine_distance, euclidean_distance
 from src.kNN import KNearestNeighbors
+
+
+def test_instantiation():
+    """
+    Basic instantiation test.
+    """
+    assert isinstance(KNearestNeighbors(3, cosine_distance), KNearestNeighbors)
