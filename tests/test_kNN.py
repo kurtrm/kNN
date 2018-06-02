0da51aa671fee71d2e3f3aa62867f246fca041ea
Kurt Maurer <kurtrm@gmail.com>
2018-06-01 21:05:14 -0700

    Added a simple test against scipy's cosine distance function.

--- tests/test_kNN.py
+++ tests/test_kNN.py
@@ -1,9 +1,15 @@
 """
 Test the kNN class and distance functions.
 """
+import numpy as np
+import pytest
+
+from scipy.spatial.distance import cosine, euclidean
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+
+
 from src.distance_funcs import cosine_distance, euclidean_distance
 from src.kNN import KNearestNeighbors
 
@@ -13,3 +19,15 @@ def test_instantiation():
     Basic instantiation test.
     """
     assert isinstance(KNearestNeighbors(3, cosine_distance), KNearestNeighbors)
+
+
+def test_cosine_dist_func():
+    """
+    Simple test for cosine distance function.
+    """
+    scratch_cosine = cosine_distance(np.array([4, 5, 6]),
+                                     np.array([7, 8, 9]))[0]
+    scipy_cosine = cosine(np.array([4, 5, 6]),
+                          np.array([7, 8, 9]))
+
+    assert scratch_cosine == pytest.approx(scipy_cosine, .01)
